5866354cf0df5197e986ad61050978f4b5de34d5
aum-metrics <aum-metrics@gmail.com>
2026-01-17 12:51:43 +0530

    chore: codebase cleanup - standardized migrations and removed orphans

--- scripts/generate_failure_report.sql
+++
@@ -1,9 +0,0 @@
--- "The Pulse of Science" Report Query
-SELECT 
-    field,
-    failure_mode,
-    COUNT(*) as frequency,
-    ROUND(AVG(score), 2) as avg_score
-FROM metadata_logs
-GROUP BY field, failure_mode
-ORDER BY frequency DESC;
